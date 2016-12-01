c1bf6ec5108daf8e8509b75ea971c4f3a1a94411
Tuhina Chatterjee <tuhinatwyla@gmail.com>
2016-12-01 16:25:03 +0530

    Update cron_time.php

--- public/cron_time.php
+++ public/cron_time.php
@@ -10,8 +10,8 @@
 			$now = time();
 			$diff = $now - $this->last;
 			if(abs($diff)== 3600) {
-				$ch = curl_init();
-				curl_setopt($ch, CURLOPT_URL, 'http://localhost:8080/cron_job.php');
+			    $ch = curl_init();
+			    curl_setopt($ch, CURLOPT_URL, 'http://localhost:8080/cron_job.php');
 			    curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
 			    curl_exec($ch);
 			    curl_close($ch);
